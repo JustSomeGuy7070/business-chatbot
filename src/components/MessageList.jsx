ddf5c59d4d2e9159d6e6a84be9be7cf84462eb47
JustSomeGuy7070 <107418641+JustSomeGuy7070@users.noreply.github.com>
2026-06-14 21:58:32 +0200

    Add automatic chat scrolling

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useRef } from "react";
 import Message from "./Message";
 
 function MessageList({ messages }) {
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+      messagesEndRef.current?.scrollIntoView({
+        behavior: "smooth",
+      });
+    }, [messages]);
+
+  useEffect(() => {
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]); 
+
   return (
     <div className="messages">
       {messages.map((message, index) => (
@@ -10,6 +23,8 @@ function MessageList({ messages }) {
           text={message.text}
         />
       ))}
+
+      <div ref={messagesEndRef} />
     </div>
   );
 }
